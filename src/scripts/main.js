9fc9cfd69c6fa4a5dd235bf6ac2138847aa61ed8
ChrisZinch <chrispakulyak@gmail.com>
2020-11-12 11:17:11 +0200

    full solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const cardButtons = document.querySelectorAll('.card__button');
+
+const secondary = (event) => {
+  event.target.classList.remove('primary');
+  event.target.classList.add('secondary');
+};
+
+cardButtons.forEach((button) => {
+  button.addEventListener('click', secondary);
+});
